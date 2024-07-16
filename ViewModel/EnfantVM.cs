09a0194028411982d3de84cc18472ff259a6011e
LakhdarGibril <gibril.lakhdar@iut-dijon.u-bourgogne.fr>
2024-07-16 17:01:05 +0200

    Ajout des premières VM

---
+++ ViewModel/EnfantVM.cs
@@ -0,0 +1,92 @@
+﻿using Model;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    /// <summary>
+    /// Représente une vue model pour la classe Enfant
+    /// </summary>
+    /// <author>Lakhdar Gibril</author>
+    public class EnfantVM : INotifyPropertyChanged
+    {
+        #region --------- Attributs ---------
+        private Enfant model;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion
+
+        #region --------- Propriétés ---------
+        /// <summary>
+        /// Permet de retourner le model d'Enfant de la vue model
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public Enfant Model => this.model;
+
+        /// <summary>
+        /// Propriété en lecture seule retournant l'identifiant de l'enfant.
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public int IdEnfant => this.model.IdEnfant;
+
+        /// <summary>
+        /// Propriété permettant de modifier ou renvoyer le nom de l'enfant.
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string Nom
+        {
+            get => this.model.Nom;
+            set
+            {
+                this.model.Nom = value;
+                NotifyPropertyChanged("Nom");
+            }
+        }
+
+        /// <summary>
+        /// Propriété permettant de modifier ou renvoyer le prénom de l'enfant.
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string Prenom
+        {
+            get => this.model.Prenom;
+            set
+            {
+                this.model.Prenom = value;
+                NotifyPropertyChanged("Prenom");
+            }
+        }
+
+        /// <summary>
+        /// Propriété permettant de modifier ou renvoyer l'âge de l'enfant.
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public int Age
+        {
+            get => this.model.Age;
+            set
+            {
+                this.model.Age = value;
+                NotifyPropertyChanged("Age");
+            }
+        }
+        #endregion
+
+        #region --------- Constructeur ---------
+        /// <summary>
+        /// Constructeur naturel de la classe EnfantVM
+        /// </summary>
+        /// <param name="model">objet Enfant servant de modèle pour la vue model</param>
+        /// <author>Lakhdar Gibril</author>
+        public EnfantVM(Enfant model)
+        {
+            this.model = model;
+        }
+        #endregion
+
+        #region --------- Observateur ---------
+        protected void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
---
+++ ViewModel/UsagerVM.cs
@@ -0,0 +1,131 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    /// <summary>
+    /// Classe UsagerVM représentant une vue model pour la classe Usager
+    /// </summary>
+    /// <author>Lakhdar Gibril</author>
+    public class UsagerVM : INotifyPropertyChanged
+    {
+        #region --------- Attributs ---------
+        private Usager model;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion
+
+        #region --------- Propriétés ---------
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer l'objet servant de model
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public Usager Model => this.model;
+
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer l'identifiant du model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public int IdUsager => this.model.IdUsager;
+
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer le nom du model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string Nom
+        {
+            get => this.model.Nom;
+            set
+            {
+                this.model.Nom = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer le prénom du model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string Prenom
+        {
+            get => this.model.Prenom;
+            set
+            {
+                this.model.Prenom = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer le genre du model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string Genre
+        {
+            get => this.model.Genre;
+            set
+            {
+                this.model.Genre = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer la date de naissance du model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public DateTime DateNaissance
+        {
+            get => this.model.DateNaissance;
+            set
+            {
+                this.model.DateNaissance = value;
+            }
+        }
+
+        /// <summary>
+        /// Propriété en lecture seule permettant de récupérer la composition familiale du model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string CompositionFamiliale
+        {
+            get => this.model.CompositionFamiliale;
+            set
+            {
+                this.model.CompositionFamiliale = value;
+            }
+        }
+
+        /// <summary>
+        /// Propriété en lecture et écriture sur le motif admission d'un model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string MotifAdmission
+        {
+            get => this.model.MotifAdmission;
+            set
+            {
+                this.model.MotifAdmission = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Propriété en lecture et écriture sur le motif admission d'un model Usager
+        /// </summary>
+        /// <author>Lakhdar Gibril</author>
+        public string DispositifSortant
+        {
+            get => this.model.DispositifSortant;
+            set
+            {
+                this.model.DispositifSortant = value;
+            }
+        }
+        #endregion
+    }
+}
